1131a73c7262f304db9254925e6e87ed544ae4ec
Óscar Llorente <llorente_oscar@hotmail.com>
2022-03-14 16:39:13 -0500

    Async await

--- src/services/ItemDataService.js
+++ src/services/ItemDataService.js
@@ -5,41 +5,41 @@ class ItemDataService {
   /**
    * @param {any} data
    */
-  create(data) {
-    return http.post(`/items`, data);
+  async create(data) {
+    return await http.post(`/items`, data);
   }
 
   /**
    * @param {any} item
    */
-  get(item) {
-    return http.get(`/items/${item}`);
+  async get(item) {
+    return await http.get(`/items/${item}`);
   }
 
   /**
    * @param {number} l
    * @param {number} p
    */
-  list(l, p) {
+  async list(l, p) {
     const limit = l ?? 10;
     const page = p ?? 1;
 
-    return http.get(`/items?isActive=true&_limit=${limit}&_page=${page}&_sort=name&_order=asc`);
+    return await http.get(`/items?isActive=true&_limit=${limit}&_page=${page}&_sort=name&_order=asc`);
   }
 
   /**
    * @param {any} item
    */
-  remove(item) {
-    return http.delete(`/items/${item}`);
+  async remove(item) {
+    return await http.delete(`/items/${item}`);
   }
 
   /**
    * @param {any} item
    * @param {any} data
    */
-  update(item, data) {
-    return http.patch(`/items/${item}`, data);
+  async update(item, data) {
+    return await http.patch(`/items/${item}`, data);
   }
 }
 
