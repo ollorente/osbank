2ccd7dac5b73212f08030c5a3afe8d925c8590ce
Óscar Llorente <llorente_oscar@hotmail.com>
2022-03-14 16:38:38 -0500

    Agregando diseño

--- src/components/AtomicDesign/Templates/HomeTemplate.vue
+++ src/components/AtomicDesign/Templates/HomeTemplate.vue
@@ -3,8 +3,31 @@
     <TheNavbar />
 
     <main class="container mx-auto p-3">
-      <router-view></router-view>
+      <div class="w-72 h-72 bg-white text-gray-900 text-center rounded-full mx-auto p-3 flex">
+        <div class="m-auto">
+          <div class="text-4xl font-bold">COP $4'578.000</div>
+          <div class="text-xl">Saldo</div>
+        </div>
+      </div>
     </main>
+
+    <footer
+      class="sticky bottom-0 w-full bg-yellow-400 border-t border-yellow-500 p-3 flex justify-between align-center"
+    >
+      <router-link
+        v-for="(link, index) in footLinks"
+        :key="index"
+        :to="{ name: link.component }"
+        class="w-1/3 text-gray-900 font-normal hover:text-white uppercase mx-auto p-0"
+      >
+        <div class="flex flex-col">
+          <span class="text-3xl text-center py-1"
+            ><i class="py-1" :class="link.icon ? link.icon : ''"></i
+          ></span>
+          <span class="text-xs text-center">{{ link.title }}</span>
+        </div></router-link
+      >
+    </footer>
   </div>
 </template>
 
@@ -16,7 +39,35 @@ export default {
   components: {
     TheNavbar,
   },
+  data() {
+    return {
+      footLinks: [
+        {
+          component: "NewItem",
+          icon: "fas fa-plus-circle",
+          title: "Servicios",
+          url: "/items/nuevo",
+        },
+        {
+          component: "NewItem",
+          icon: "fas fa-plus-circle",
+          title: "Nuevo",
+          url: "/items/nuevo",
+        },
+        {
+          component: "NewItem",
+          icon: "fas fa-plus-circle",
+          title: "Perfil",
+          url: "/items/nuevo",
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+main {
+  min-height: calc(100vh - 15rem);
+}
+</style>
--- src/views/estimate/NewEstimate.vue
+++ src/views/estimate/NewEstimate.vue
@@ -1,9 +1,160 @@
-<script>
-// @ts-check
-</script>
-
 <template>
-	<div class="w-full bg-white rounded my-1 p-3">Nuevo Presupuesto</div>
+  <div class="relative w-full min-h-screen bg-yellow-400 font-sans">
+    <InternalNavbar :link="headerLinks" />
+
+    <main class="container mx-auto p-3">
+      <section class="">
+        <form @submit.prevent="addEstimate">
+          <div class="py-2">
+            <label for="name" class="font-semibold ml-3">Nombre *</label>
+            <input
+              type="text"
+              class="w-full bg-white rounded p-2"
+              id="name"
+              v-model="estimate.name"
+              placeholder="Nombre *"
+              autofocus
+              required
+            />
+          </div>
+          <div class="py-2">
+            <label for="icon" class="font-semibold ml-3">Tipo</label>
+            <select
+              name="icon"
+              v-model="estimate.icon"
+              id="icon"
+              class="w-full bg-white rounded p-2"
+            >
+              <option value="null" selected>-- Seleccione un tipo --</option>
+              <option
+                v-for="(icon, index) in icons"
+                :key="index"
+                :value="icon.icon"
+              >
+                {{ icon.title }}
+              </option>
+            </select>
+          </div>
+          <button class="w-full bg-gray-900 text-white rounded my-1 p-2">
+            Agregar
+          </button>
+        </form>
+
+        <pre class="container hidden">{{ $data }}</pre>
+      </section>
+    </main>
+
+    <footer
+      class="sticky bottom-0 w-full bg-yellow-400 border-t border-yellow-500 p-3 flex justify-between align-center"
+    >
+      <router-link
+        v-for="(link, index) in footLinks"
+        :key="index"
+        :to="{ name: link.component }"
+        class="w-1/3 text-gray-900 font-normal hover:text-white uppercase mx-auto p-0"
+      >
+        <div class="flex flex-col">
+          <span class="text-3xl text-center py-1"
+            ><i class="py-1" :class="link.icon ? link.icon : ''"></i
+          ></span>
+          <span class="text-xs text-center">{{ link.title }}</span>
+        </div></router-link
+      >
+    </footer>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+// @ts-check
+// @ts-ignore
+import EstimateDataService from "@/services/EstimateDataService";
+// @ts-ignore
+import EstimateInterface from "@/interfaces/EstimateInterface.js";
+// @ts-ignore
+import InternalNavbar from "@/components/AtomicDesign/Organisms/InternalNavbar.vue";
+
+export default {
+  components: {
+    InternalNavbar,
+  },
+  data() {
+    return {
+      estimate: {
+        name: null,
+        icon: null,
+      },
+      icons: [
+        {
+          icon: "fas fa-lightbulb",
+          title: "Energía",
+        },
+        {
+          icon: "fas fa-burn",
+          title: "Gas",
+        },
+        {
+          icon: "fas fa-broadcast-tower",
+          title: "Internet",
+        },
+        {
+          icon: "fas fa-money-bill-wave",
+          title: "Pago",
+        },
+        {
+          icon: "fas fa-medkit",
+          title: "Salud",
+        },
+        {
+          icon: "fas fa-phone",
+          title: "Teléfono",
+        },
+      ],
+      footLinks: [
+        {
+          component: "Estimates",
+          icon: "fas fa-backward",
+          title: "Volver",
+          url: "/presupuesto",
+        },
+      ],
+      headerLinks: {
+        icon: "fas fa-backward",
+        title: "Nuevo presupuesto",
+      },
+    };
+  },
+  methods: {
+    // @ts-ignore
+    async addEstimate() {
+      try {
+        if (this.estimate.name === null) {
+          alert("Nombre no puede estar vacio.");
+          return;
+        }
+
+        const estimate = await EstimateInterface(this.estimate);
+
+        const { data, status } = await EstimateDataService.create(estimate)
+          .then(async (response) => {
+            return await response;
+          })
+          .catch((error) => console.log(error));
+
+        if (status !== 201) {
+          console.log(data);
+        }
+
+        await this.$router.push({ name: "Estimates" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+main {
+  min-height: calc(100vh - 12.8rem);
+}
+</style>
