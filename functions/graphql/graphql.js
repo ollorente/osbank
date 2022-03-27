6f18d6e8721215dcca438bffeab18f662f9dcf23
Óscar Llorente <llorente_oscar@hotmail.com>
2022-03-26 22:52:39 -0500

    Quitando playground

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,6 +1,10 @@
 // @ts-check
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-lambda");
+const {
+  ApolloServerPluginLandingPageDisabled,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
 const { typeDefs, resolvers } = require("./schema");
 
 require("./database");
@@ -8,6 +12,11 @@ require("./database");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 });
 
 const serverHandler = server.createHandler();
