0f118eba32309d7af52c9a790fe932042b130fa2
Óscar Llorente <llorente_oscar@hotmail.com>
2022-05-12 00:50:07 -0500

    Quitando archivos

--- src/main.js
+++ src/main.js
@@ -2,12 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
-// import VueApollo from 'vue-apollo'
-
-// App.use(VueApollo);
-
-// const apolloProvider = new VueApollo({
-//   defaultClient: apolloClient,
-// });
 
 createApp(App).use(router).use(store).mount('#app')
